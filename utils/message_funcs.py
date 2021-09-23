1b1e671567c802046653f618eaa05a75ff4a94d4
Shlok <wizard1net@gmail.com>
2021-09-23 18:55:28 +0530

    Filtered hi and bye to only respond when word detected at end of sentence.

--- utils/message_funcs.py
+++ utils/message_funcs.py
@@ -3,10 +3,10 @@ import re
 
 
 async def greeting(ctx: Context):
-    if re.search(r'^\b(hi|hello|hey)\b', ctx.message.content.lower()):
+    if re.search(r'^\b(hi|hello|hey)\b$', ctx.message.content.lower()):
         await ctx.reply(f"Hello {ctx.author.mention}!")
 
 
 async def farewell(ctx: Context):
-    if re.search(r'^\b(bye|(see|c) ya (later)?)', ctx.message.content.lower()):
+    if re.search(r'^\b(bye|(see|c) ya (later)?)\b$', ctx.message.content.lower()):
         await ctx.reply(f"Bye {ctx.author.mention}! Come back soon!")
