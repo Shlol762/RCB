afef1893f64a50639211dcc383aff257db610528
cricketrcb <iamaaroh@gmail.com>
2022-04-02 13:14:58 +0530

    teams

--- utils/parser.py
+++ utils/parser.py
@@ -12,5 +12,6 @@ class IPLT20:
             soup = BeautifulSoup(req.text, 'lxml')
         self.matches_url = soup.find(href=re.compile(r'matches/schedule/men')).get('href')
         self.stats_url = soup.find(href=re.compile(r'stats/')).get('href')
+        self.teams_url = soup.find(href=re.compile(r'teams/men')).get('href')
 
 IPLT20()
